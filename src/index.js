8284fe55351a2b5f9538940d5b608e75cc220a52
Farah Khan <farahkhanu@gmail.com>
2018-08-28 21:24:49 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return (
+        <div>
+        <h1>Hello World!!!</h1>
+        <h2>Hey</h2>
+        </div>
+        )
+};
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
